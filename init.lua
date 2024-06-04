74d7091282a01a64cd42fe37a4f561e51039bcab
Lukasz <lukaszprzybylka@gmail.com>
2024-06-04 20:51:24 +0100

    attempt to modularise config

--- init.lua
+++ init.lua
@@ -504,7 +504,8 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
+        'csharpier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
